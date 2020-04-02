ab528f5f59f723cd1949bcc3c5e48fc173077075
aaltodave <63040263+aaltodave@users.noreply.github.com>
2020-04-02 11:25:59 +0300

    Added the website (and css+php form)
    Added html file and the css file with the php form

---
+++ lomake.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+	$ni = $_POST["firstname"]; 
+	echo $ni; 
+	
+	echo "<br>";
+
+	$nis = $_POST["lastname"]; 
+	echo $nis;
+	
+	echo "<br>";
+	
+	$fac = $_POST["faction"];
+	echo $fac;
+	
+	echo "<br>";
+	
+	$veh = $_POST["vehicle"];
+	echo $veh;
+	
+	echo "<br>";
+	
+	$exp = $_POST["expansion"];
+	echo $exp;
+	echo "<br>";
+?>
+
